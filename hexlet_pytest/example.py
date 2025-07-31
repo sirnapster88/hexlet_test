edb4ba3cd525eb8a68ba13f1de85a71450514c27
sirnapster88 <sir.napster@yandex.ru>
2025-07-31 16:09:58 +0300

    adding test

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,10 @@
 def reverse(string):
     return string[::-1]
+
+
+def read_file():
+    text = open('d:/study/hexlet_pytest/tests/test_data/normal_text.txt').read()
+    return text
+
+
+print(reverse(read_file()))
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,7 @@
+import sys
+sys.path[0] = 'd:\study\hexlet_pytest'
+from pathlib import Path
 from hexlet_pytest.example import reverse
-
 
 def test_reverse():
     assert reverse("Hexlet") == "telxeH"
@@ -7,3 +9,19 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse("") == ""
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / "test_data" / filename
+
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+
+def test_process():
+    test_data = read_file("normal_text.txt")
+    expected = read_file("reverse_text.txt")
+    actual = reverse(test_data)
+
+    assert actual == expected
